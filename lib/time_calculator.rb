a74255eec848cd5f470a8d76aee592f5c1280fe0
Wardah <wlt_93@hotmail.com>
2019-06-03 16:58:57 +0100

    Time taken to run 'reverse' method of array

--- lib/time_calculator.rb
+++ lib/time_calculator.rb
@@ -4,4 +4,19 @@ class TimeCalculator
     @array = 5000.times.map{ Random.rand(10000) }
   end
 
+  def run_program
+    @start_time = Time.now.nsec
+    @array.reverse!
+    @end_time = Time.now.nsec
+  end
+
+  def total_time_taken
+    puts @end_time - @start_time
+  end
+
 end
+
+time_calculator = TimeCalculator.new
+time_calculator.array_generator
+time_calculator.run_program
+time_calculator.total_time_taken
--- spec/time_calculator_spec.rb
+++ spec/time_calculator_spec.rb
@@ -7,4 +7,13 @@ describe TimeCalculator do
     expect(time_calculator.array_generator).to be_instance_of(Array)
   end
 
+  it "checks for time taken to run 'reverse' method on the array" do
+    time_calculator = TimeCalculator.new
+    time_calculator.array_generator
+    time_calculator.run_program
+    allow(time_calculator).to receive(:total_time_taken) {4000}
+
+    expect(time_calculator.total_time_taken).to eq 4000
+  end
+
 end
